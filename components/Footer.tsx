f70c16fcb1e43679f75d6da9b9388f819ef2cf9e
Yeabsira Mekuria <yabumek46@gmail.com>
2024-01-05 21:54:20 +0300

    feat: add footer

---
+++ components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <p className="text-center text-xs text-black">
+          &copy; 2023 Ecommerce Website, Inc. All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
