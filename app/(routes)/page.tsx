e114ad628ec3d374c8f5b732d2f1988d6a986fbe
Yeabsira Mekuria <yabumek46@gmail.com>
2024-01-05 23:20:13 +0300

    add cover page for the store

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import getBillboard from "@/actions/get-billboards";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/Container";
 
-const HomePage = () => {
-  return <div>HomePage</div>;
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const billboard = await getBillboard("clqu4vg5300017x9t8xrte94c");
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+      </div>
+    </Container>
+  );
 };
 
 export default HomePage;
