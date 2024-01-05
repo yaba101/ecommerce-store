6e8412b95918e28eab327d95a7533d39712b9786
Yeabsira Mekuria <yabumek46@gmail.com>
2024-01-05 23:23:03 +0300

    update types

--- types.ts
+++ types.ts
@@ -9,3 +9,31 @@ export interface Category {
   name: string;
   billboard: Billboard;
 }
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: string;
+  isFeatured: boolean;
+  size: Size;
+  colro: Color;
+  images: Image[];
+}
+
+export interface Color {
+  id: string;
+  name: string;
+  value: string;
+}
+
+export interface Image {
+  id: string;
+  url: string;
+}
+
+export interface Size {
+  id: string;
+  name: string;
+  value: string;
+}
